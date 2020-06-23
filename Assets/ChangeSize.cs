388b1a585c7a68e5ff6bf3de2c1bc4572c04066d
Zhuoyue Lyu <zhuoyue.lyu@gmail.com>
2020-06-22 22:57:43 -0400

    initial commit

---
+++ Assets/ChangeSize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSize : MonoBehaviour
+{
+    public float scale = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AdjustSize(float newScale) {
+
+        this.transform.localScale = new Vector3(newScale, newScale, newScale);
+
+    }
+}
---
+++ Assets/nyChange.cs
@@ -0,0 +1,73 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class nyChange : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean bigger;
+
+        public SteamVR_Action_Boolean smaller;
+
+        public Hand hand;
+
+        public Transform handToResize;
+
+
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            bigger.AddOnChangeListener(OnBiggerActionChange, hand.handType);
+            smaller.AddOnChangeListener(OnSmallerActionChange, hand.handType);
+        }
+
+        private void OnDisable()
+        {
+            if (bigger != null)
+                bigger.RemoveOnChangeListener(OnBiggerActionChange, hand.handType);
+            if (smaller != null)
+                smaller.RemoveOnChangeListener(OnSmallerActionChange, hand.handType);
+        }
+
+        private void OnBiggerActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+        {
+            if (newValue)
+            {
+                handToResize.localScale += new Vector3(1, 1, 1);
+            }
+        }
+
+        private void OnSmallerActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+        {
+            if (newValue)
+            {
+                handToResize.localScale -= new Vector3(1, 1, 1);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+     
